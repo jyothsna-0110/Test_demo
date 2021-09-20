42a6c346db2971019e585ca416a68d30d7127e89
jyothsna-0110 <jyothsnachennuri1313@gmail.com>
2021-09-20 12:52:44 +0530

    commit

---
+++ udemy_lectures/coding.js
@@ -0,0 +1,97 @@
+/*let weightMark = 78;
+let heightMark = 1.69;
+
+let weightMark2 = 95;
+let heightMark2 = 1.88;
+
+let weightJohn = 92;
+let heightJohn = 1.95;
+
+let weightJohn2 = 85;
+let heightJohn2 = 1.76;
+
+
+let markBMI = weightMark / heightMark ** 2;
+let johnBMI = weightJohn / heightJohn ** 2;
+
+let markBMI2 = weightMark2 / heightMark2 ** 2;
+let johnBMI2 = weightJohn2 / heightJohn2 ** 2;
+
+console.log("BMI of Mark is : "+markBMI);
+console.log("BMI of John is : "+johnBMI);
+console.log("BMI of Mark2 is : "+markBMI2);
+console.log("BMI of John2 is : "+johnBMI2);
+
+let markHigherBMI;
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+if(markBMI > johnBMI){
+    console.log(`mark's BMI is  Higherthan the john's BMI `);
+}
+else{
+    console.log(`John's BMI is Higherthan the Mark's BMI`);
+
+}
+
+markHigherBMI = markBMI2 > johnBMI2;
+console.log(markHigherBMI);
+
+if(markBMI2 > johnBMI2){
+    console.log(`mark's BMI is  Higherthan the john's BMI `);
+}
+else{
+    console.log(`John's BMI is Higherthan the Mark's BMI`);
+
+
+}*/
+
+// coding challenge 3
+// const scoreDolphins=(96+108+89)/3;
+// const scoreKoalas=(88+91+110)/3;
+// console.log(scoreDolphins);
+// console.log(scoreKoalas);
+
+// if(scoreKoalas<scoreDolphins){
+//     console.log("Dolphins team is the winner");
+// } else if(scoreKoalas>scoreDolphins){
+//     console.log("Koalas team is the winner");
+// }
+// else if(scoreKoalas===scoreDolphins){
+//     console.log("both teams win the trophy");
+// }
+
+// const scoreDolphins=(97+112+101)/3;
+// const scoreKoalas=(109+95+123)/3;
+// console.log(scoreDolphins,scoreKoalas);
+// if(scoreKoalas<scoreDolphins && scoreDolphins>=100){
+//     console.log("Dolphins team is the winner");
+// } else if(scoreKoalas>scoreDolphins && scoreKoalas>=100){
+//     console.log("Koalas team is the winner");
+// }
+// else if(scoreKoalas===scoreDolphins){
+//     console.log("both teams win the trophy");
+// }
+
+// const scoreDolphins=(97+112+80)/3;
+// const scoreKoalas=(109+95+90)/3;
+// console.log(scoreDolphins,scoreKoalas);
+// if(scoreKoalas<scoreDolphins && scoreDolphins>=100){
+//     console.log("Dolphins team is the winner");
+// } else if(scoreKoalas>scoreDolphins && scoreKoalas>=100){
+//     console.log("Koalas team is the winner");
+// }
+// else if(scoreKoalas===scoreDolphins && scoreKoalas>=100 && scoreDolphins>=100){
+//     console.log("both teams win the trophy");
+// }
+// else{
+//     console.log("No one win the trophy");
+
+// }
+
+
+//coding challenge 4
+
+let bill=430;
+let tip;
+console.log(`The bill is ${bill}, the tip is ${tip=bill<=300 && tip>=50?bill*(15/100):bill*(20/100)} and total bill is ${bill+tip}  `);
